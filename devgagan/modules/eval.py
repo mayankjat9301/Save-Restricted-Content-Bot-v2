7bdab2fa7e62a88a765a487e3dc698188ea3b3ed
mayankjat9301 <mayankjat9301@gmail.com>
2026-02-06 21:08:03 +0530

    Update eval.py

--- devgagan/modules/eval.py
+++ devgagan/modules/eval.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------
-# File Name: eval.py 
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Unknown
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import os, re, subprocess, sys, traceback
 from inspect import getfullargspec
 from io import StringIO
