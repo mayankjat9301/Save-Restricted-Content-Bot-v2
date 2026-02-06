48808ccea6fb896fc4e868f9aa818a0c847dea1a
mayankjat9301 <mayankjat9301@gmail.com>
2026-02-06 21:11:54 +0530

    Update plans_db.py

--- devgagan/core/mongo/plans_db.py
+++ devgagan/core/mongo/plans_db.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------
-# File Name: plans_db.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import datetime
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB
